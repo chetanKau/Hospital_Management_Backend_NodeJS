337869a257ada308c3cf9e166198a3182418b7b8
Chetan kaushik <chetankaushik29@gmail.com>
2024-06-21 23:31:43 +0530

    app.js file cors are updated with new code

--- app.js
+++ app.js
@@ -5,19 +5,43 @@ import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 import { dbConnection } from "./database/dbConnection.js";
 import messageRouter from "./router/messageRouter.js";
-import {errorHandler} from "./middlewares/errorMiddleware.js";
+import { errorHandler } from "./middlewares/errorMiddleware.js";
 import userRouter from "./router/userRouter.js"
 import appointmentRouter from "./router/appointmentRouter.js"
 
 const app = express();
 
-config()
+// config()
+
+// app.use(cors({
+//     origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true
+//   }));
+
+
+
+const allowedOrigins = [process.env.FRONTEND_URL, process.env.DASHBOARD_URL];
 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true
-}))
+}));
+
+// Handling preflight requests
+app.options('*', cors());
+
+
+
 
 app.use(cookieParser());
 app.use(express.json());
@@ -28,8 +52,8 @@ app.use(fileUpload({
 }));
 
 app.use("/api/v1/message", messageRouter)
-app.use("/api/v1/user",userRouter);
-app.use("/api/v1/appointment",appointmentRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/appointment", appointmentRouter);
 
 
 
