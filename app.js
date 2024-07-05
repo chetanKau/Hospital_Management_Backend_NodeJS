686f383f8e7919fd45d7fb38a1af21bc7fda6656
Chetan kaushik <chetankaushik29@gmail.com>
2024-07-05 15:34:58 +0530

    C:/Program Files/Git/ removed from APIs

--- app.js
+++ app.js
@@ -13,29 +13,29 @@ const app = express();
 
 config()
 
-// app.use(cors({
-//     origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true
-//   }));
-
-
-
-const allowedOrigins = [process.env.FRONTEND_URL, process.env.DASHBOARD_URL];
-
 app.use(cors({
-    origin: function (origin, callback) {
-        // Allow requests with no origin (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+    // methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
-}));
+  }));
+
+
+
+// const allowedOrigins = [process.env.FRONTEND_URL, process.env.DASHBOARD_URL];
+
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         // Allow requests with no origin (like mobile apps or curl requests)
+//         if (!origin) return callback(null, true);
+//         if (allowedOrigins.indexOf(origin) === -1) {
+//             const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+//             return callback(new Error(msg), false);
+//         }
+//         return callback(null, true);
+//     },
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     credentials: true
+// }));
 
 // Handling preflight requests
 app.options('*', cors());
