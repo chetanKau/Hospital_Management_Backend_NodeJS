949d2e94f147ff86e801f6f99dbc454bb2e46b70
Chetan kaushik <chetankaushik29@gmail.com>
2024-07-05 15:39:05 +0530

    some changes

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ config()
 
 app.use(cors({
     origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
-    // methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   }));
 
